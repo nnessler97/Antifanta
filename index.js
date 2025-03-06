95a22a778450598a5743c2155fc534ca07733292
nnessler97 <nico.nessler@gmx.de>
2025-03-06 20:57:20 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import PublicBrandWebsite from "@/components/PublicBrandWebsite"; // Stelle sicher, dass der Pfad korrekt ist.
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <PublicBrandWebsite />
+    </div>
+  );
+}
